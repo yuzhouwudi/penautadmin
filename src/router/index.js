2652cb8589bbf67865067f65c86cc64685cc0f1e
weirdo <1061420168qq.com>
2018-05-23 11:21:19 +0800

    'penautadmin'

--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,9 @@ import Login from '@/components/login/login'
 import G from '@/components/goods/goods'
 import O from '@/components/order/order'
 import M from '@/components/manager/manager'
-<<<<<<< HEAD
-import U from '@/components/user/user'
-=======
+
 import User from '@/components/user/user'
->>>>>>> a603c9a11fe6d5609c49aff00021f28b4a7027c9
+
 
 Vue.use(Router);
 
@@ -19,58 +17,34 @@ export default new Router({
       path: '/',
       name: 'main',
       component: Main,
-<<<<<<< HEAD
-      redirect: '/user',
-      children: [
 
+      redirect: '/user',
+
+      children: [
         {
-          path: "goods",
+          path: "/goods",
           component: G,
         },
         {
-          path: "manager",
+          path: "/manager",
           component: M,
         },
         {
-          path: "order",
+          path: "/order",
           component: O,
         },
         {
-          path: "user",
-          component: U,
+          path: "/user",
+          component: User,
         },
       ]
     },
+
+
     {
       path: "/login",
       component: Login,
     },
-=======
-        children:[
-            {
-                path:"/goods",
-                component:G,
-            },
-            {
-                path:"/manager",
-                component:M,
-            },
-            {
-                path:"/order",
-                component:O,
-            },
-            {
-                path:"/user",
-                component:User,
-            },
-        ]
-    },
 
-
-      {
-          path:"/login",
-          component:Login,
-      },
->>>>>>> a603c9a11fe6d5609c49aff00021f28b4a7027c9
   ]
 })
